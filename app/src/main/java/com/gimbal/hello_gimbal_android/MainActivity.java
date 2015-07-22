989ee45fdc78f78f23e6ec5708da99ed1b93f0d6
zzhengz <zarickzheng@gmail.com>
2015-07-22 11:04:00 -0500

    Room detection

--- app/src/main/java/com/gimbal/hello_gimbal_android/MainActivity.java
+++ app/src/main/java/com/gimbal/hello_gimbal_android/MainActivity.java
@@ -25,6 +25,9 @@ public class MainActivity extends ActionBarActivity {
     private BeaconManager beaconManager;
     private TextView mainText1;
     private TextView mainText2;
+    private int rssi1;
+    private int rssi2;
+    private int rssi3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +35,8 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         mainText1=(TextView) findViewById(R.id.editText);
         mainText2=(TextView) findViewById(R.id.editText2);
+        mainText1.setText("No beacon sighted");
+        mainText2.setText(String.format("current RSSI unavailable"));
         Gimbal.setApiKey(this.getApplication(), "17ec8de5-fa08-47db-b399-cff8a44c64ac");
 /*
         placeEventListener = new PlaceEventListener() {
@@ -57,8 +62,26 @@ public class MainActivity extends ActionBarActivity {
         beaconEventListener = new BeaconEventListener(){
 
             public void onBeaconSighting(BeaconSighting sighting) {
-                mainText1.setText("We are in room 1");
-                mainText2.setText(String.format("current RSSI = %d", sighting.getRSSI()));
+                String myStr=sighting.getBeacon().getName();
+                int mRSSI=sighting.getRSSI();
+                if(myStr.equals("beacon 1")) {
+                    rssi1 = Math.abs(mRSSI);
+                }else if(myStr.equals("beacon 2")) {
+                    rssi2 = Math.abs(mRSSI);
+                }else if(myStr.equals("beacon 3")) {
+                    rssi3 = Math.abs(mRSSI);
+                }
+
+                if(rssi1!=0 && rssi1 <60){
+                    mainText1.setText("We are in room 1");
+                    mainText2.setText(String.format("current RSSI = -%d", rssi1));
+                }else if(rssi2!=0 && rssi2 <60){
+                    mainText1.setText("We are in room 2");
+                    mainText2.setText(String.format("current RSSI = -%d", rssi2));
+                }else if(rssi3!=0 && rssi3 <60){
+                    mainText1.setText("We are in room 3");
+                    mainText2.setText(String.format("current RSSI = -%d", rssi3));
+                }
                 // This will be invoked upon beacon sighting
             }
             // add BeaconEventListener method here
